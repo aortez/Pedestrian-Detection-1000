71ffdfba010b9c36d1ddf465762a58ffac64251a
Allan Ortiz <aortez@gmail.com>
2011-11-08 09:09:57 -0800

    * Added missing cvaux lib to project, but it still isn't compiling yet. * Started adding orientation computation.
    I think there are numerous questions about data types that we'll want
    to use, but it probably won't make sense until we can actually
    run it and look at the output.  Another thought, we might want to
    perform the entire algorithm on a single color channel image
    first, and then take the step to doing RGB.

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,29 @@ int main( int argc, const char* argv[] )
     // read input file
     Mat src = imread( argv[ 1 ], 1 );
 
-    // Gradient computation
-    Mat kernel_horz = Mat( 1, 3, CV_8UC1, Scalar( -1, 0, 1 ) ); // [-1, 0, 1]
-    Mat kernel_vert = Mat( 3, 1, CV_8UC1, Scalar( -1, 0, 1 ) ); // [-1, 0, 1]^T
-    Mat gradient_horz;
-    Mat gradient_vert;
-    filter2D( src, gradient_horz, -1, kernel_horz, Point( -1, -1 ) );
-    filter2D( src, gradient_vert, -1, kernel_vert, Point( -1, -1 ) );
+    // compute gradient images
+    Mat gradient_horz( src.size(), CV_64FC1 );
+    Mat gradient_vert( src.size(), CV_64FC1 );
+    {
+        Mat kernel_horz = Mat( 1, 3, CV_64FC1, Scalar( -1, 0, 1 ) ); // [-1, 0, 1]
+        Mat kernel_vert = Mat( 3, 1, CV_64FC1, Scalar( -1, 0, 1 ) ); // [-1, 0, 1]^T
+        filter2D( src, gradient_horz, -1, kernel_horz );
+        filter2D( src, gradient_vert, -1, kernel_vert );
+    }
+
+    // compute orientation image
+    Mat orientation( gradient_horz.size(), CV_64FC1 );
+    {
+        for ( int y = 0; y < orientation.rows; y++ )
+        {
+            for ( int x = 0; x < orientation.cols; x++ )
+            {
+                const double h = gradient_horz.at< double >( y, x );
+                const double v = gradient_vert.at< double >( y, x );
+                orientation.at< double >( y, x ) = atan2( v, h );
+            }
+        }
+    }
 
     // Debug: Show progress.
     namedWindow( "Progress", 1 );
