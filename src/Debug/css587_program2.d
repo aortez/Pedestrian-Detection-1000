b4179939620f66ab30ad3188fc2ad5fb4b82abea
Allan Ortiz <aortez@gmail.com>
2011-11-08 22:28:14 -0800

    Project is now running but gradient filtered images aren't correct.

---
+++ src/Debug/css587_program2.d
@@ -0,0 +1,38 @@
+css587_program2.d: ../css587_program2.cpp /usr/include/opencv/cv.h \
+ /usr/include/opencv/cxcore.h /usr/include/opencv/cxtypes.h \
+ /usr/include/opencv/cxerror.h /usr/include/opencv/cvver.h \
+ /usr/include/opencv/cxcore.hpp /usr/include/opencv/cxmisc.h \
+ /usr/include/opencv/cxoperations.hpp /usr/include/opencv/cxmat.hpp \
+ /usr/include/opencv/cxflann.h /usr/include/opencv/cvtypes.h \
+ /usr/include/opencv/cv.hpp /usr/include/opencv/cvcompat.h \
+ /usr/include/opencv/highgui.h /usr/include/opencv/highgui.hpp
+
+/usr/include/opencv/cv.h:
+
+/usr/include/opencv/cxcore.h:
+
+/usr/include/opencv/cxtypes.h:
+
+/usr/include/opencv/cxerror.h:
+
+/usr/include/opencv/cvver.h:
+
+/usr/include/opencv/cxcore.hpp:
+
+/usr/include/opencv/cxmisc.h:
+
+/usr/include/opencv/cxoperations.hpp:
+
+/usr/include/opencv/cxmat.hpp:
+
+/usr/include/opencv/cxflann.h:
+
+/usr/include/opencv/cvtypes.h:
+
+/usr/include/opencv/cv.hpp:
+
+/usr/include/opencv/cvcompat.h:
+
+/usr/include/opencv/highgui.h:
+
+/usr/include/opencv/highgui.hpp:
--- src/main.cpp
+++ src/main.cpp
@@ -5,47 +5,58 @@ using namespace cv;
 using namespace std;
 
 /**
+ * Process the input image to determine if it contains a person.
+ */
+bool containsPerson( const Mat& img );
+
+
+/**
  * Entry point to app. Loads image from file system and feeds it through the algorithm.
  */
 int main( int argc, const char* argv[] )
 {
-    if( argc != 2 )
-    {
-        cerr << "Please provide a path to an image" << endl;
-        return -1;
-    }
 
-    // read input file
-    Mat src = imread( argv[ 1 ], 1 );
+    Mat src = imread( "test.png" );
+//    Mat src = imread( argv[ 1 ] );
 
+    cout << "Is a person: " << boolalpha << containsPerson( src ) << endl;
+    return 0;
+}
+
+bool containsPerson( const Mat& src )
+{
+    namedWindow( "input" );
+    imshow( "input", src );
+    
     // compute gradient images
-    Mat gradient_horz( src.size(), CV_64FC1 );
-    Mat gradient_vert( src.size(), CV_64FC1 );
+    Mat gradient_horz( src.size(), src.type() );
+    Mat gradient_vert( src.size(), src.type() );
     {
         Mat kernel_horz = Mat( 1, 3, CV_64FC1, Scalar( -1, 0, 1 ) ); // [-1, 0, 1]
-        Mat kernel_vert = Mat( 3, 1, CV_64FC1, Scalar( -1, 0, 1 ) ); // [-1, 0, 1]^T
+        Mat kernel_vert = Mat( 3, 1, CV_8SC1, Scalar( -1, 0, 1 ) ); // [-1, 0, 1]^T
         filter2D( src, gradient_horz, -1, kernel_horz );
         filter2D( src, gradient_vert, -1, kernel_vert );
     }
+    namedWindow( "hgrad" );
+    imshow( "hgrad", gradient_horz );
+
+    namedWindow( "vgrad" );
+    imshow( "vgrad", gradient_vert );
+    waitKey();
 
     // compute orientation image
-    Mat orientation( gradient_horz.size(), CV_64FC1 );
-    {
-        for ( int y = 0; y < orientation.rows; y++ )
-        {
-            for ( int x = 0; x < orientation.cols; x++ )
-            {
-                const double h = gradient_horz.at< double >( y, x );
-                const double v = gradient_vert.at< double >( y, x );
-                orientation.at< double >( y, x ) = atan2( v, h );
-            }
-        }
-    }
-
-    // Debug: Show progress.
-    namedWindow( "Progress", 1 );
-    imshow( "Progress", src );
-    waitKey();
+//    Mat orientation( gradient_horz.size(), CV_64FC1 );
+//    {
+//        for ( int y = 0; y < orientation.rows; y++ )
+//        {
+//            for ( int x = 0; x < orientation.cols; x++ )
+//            {
+//                const double h = gradient_horz.at< double >( y, x );
+//                const double v = gradient_vert.at< double >( y, x );
+//                orientation.at< double >( y, x ) = atan2( v, h );
+//            }
+//        }
+//    }
 
     // Histograms -- Orientation Binning
 
@@ -55,5 +66,5 @@ int main( int argc, const char* argv[] )
 
     // SVM classification
 
-    return 0;
+    return false;
 }
