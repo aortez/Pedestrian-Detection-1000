38f171b4d21629d3227223686c8864f56ec5ef62
Allan Ortiz <aortez@gmail.com>
2011-11-09 18:53:49 -0800

    * Input image is now converted to floating point after initial conversion to b/w. * Added computation for gradient magnitude.  These are used as the weights for each orientation pixel.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
+#include <cstdio>
 #include <cv.h>
 #include <highgui.h>
 #include <iostream>
+#include <sstream>
 using namespace cv;
 using namespace std;
 
-/**
- * Process the input image to determine if it contains a person.
- */
+
 bool containsPerson( const Mat& img );
 
+string toString( const Mat& img )
+{
+    stringstream ss;
+    ss << "cols: " << img.cols << endl;
+    ss << "rows: " << img.rows << endl;
+    ss << "type: " << img.type() << endl;
+    ss << "depth: " << img.depth() << endl;
+    return ss.str();
+}
+
+void show( const Mat& img, const string& windowName )
+{
+    namedWindow( windowName.c_str() );
+    imshow( windowName.c_str(), img );
+}
 
 /**
  * Entry point to app. Loads image from file system and feeds it through the algorithm.
@@ -25,45 +40,50 @@ int main( int argc, const char* argv[] )
 
 bool containsPerson( const Mat& src )
 {
-    namedWindow( "input" );
-    imshow( "input", src );
+    show( src, "input" );
 
     // convert to greyscale
     Mat bwSrc;
     cvtColor( src, bwSrc, CV_RGB2GRAY );
+
+    // convert to floating point
+    bwSrc.convertTo( bwSrc, CV_64F, 1.0 / 255 );
     
     // compute gradient images
-    Mat gradient_horz( bwSrc.size(), bwSrc.type() );
-    Mat gradient_vert( bwSrc.size(), bwSrc.type() );
+    Mat gradient_horz( bwSrc.size(), CV_64F );
+    Mat gradient_vert( bwSrc.size(), CV_64F );
     {
         Mat kernel_horz = ( Mat_<double>( 1, 3 ) << -1, 0, 1 );
         Mat kernel_vert = ( Mat_<double>( 3, 1 ) << -1, 0, 1 );
         filter2D( bwSrc, gradient_horz, -1, kernel_horz );
         filter2D( bwSrc, gradient_vert, -1, kernel_vert );
     }
-    namedWindow( "bw" );
-    imshow( "bw", bwSrc );
 
-    namedWindow( "hgrad" );
-    imshow( "hgrad", gradient_horz );
+    // compute orientation and orientation magnitude image
+    Mat orientation( gradient_horz.size(), CV_64F );
+    Mat magnitude( gradient_horz.size(), CV_64F );
+    {
+        for ( int y = 0; y < orientation.rows; y++ )
+        {
+            for ( int x = 0; x < orientation.cols; x++ )
+            {
+                const double h = gradient_horz.at< double >( y, x );
+                const double v = gradient_vert.at< double >( y, x );
+                const double angle = atan2( v, h );
+                const double mag = sqrt( v * v + h * h );
+                orientation.at< double >( y, x ) = angle;
+                magnitude.at< double >( y, x ) = mag;
+                printf( "@(%d,%d): (h,v) = (%f, %f), angle = %f, magnitude = %f\n", x, y, h, v, angle, mag );
+            }
+        }
+    }
 
-    namedWindow( "vgrad" );
-    imshow( "vgrad", gradient_vert );
+    show( bwSrc, "bwSrc" );
+    show( gradient_horz, "hgrad" );
+    show( gradient_vert, "vgrad" );
+    show( orientation, "orientation" );
+    show( magnitude, "magnitude" );
     waitKey();
-
-    // compute orientation image
-//    Mat orientation( gradient_horz.size(), CV_64FC1 );
-//    {
-//        for ( int y = 0; y < orientation.rows; y++ )
-//        {
-//            for ( int x = 0; x < orientation.cols; x++ )
-//            {
-//                const double h = gradient_horz.at< double >( y, x );
-//                const double v = gradient_vert.at< double >( y, x );
-//                orientation.at< double >( y, x ) = atan2( v, h );
-//            }
-//        }
-//    }
 
     // Histograms -- Orientation Binning
 
