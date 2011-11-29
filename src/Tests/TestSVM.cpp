595288766aac9d13add6821e7a7e7bb21d231a40
Allan Ortiz <aortez@gmail.com>
2011-11-29 09:57:02 -0800

    disabled non-working SVM test

--- src/Tests/TestSVM.cpp
+++ src/Tests/TestSVM.cpp
@@ -12,10 +12,10 @@ SUITE( SVMTest )
         Mat trainClass1 = ( Mat_< double >( 1, 4 ) << -10, -9, -6, -0.0000001 );
         Mat trainClass2 = ( Mat_< double >( 1, 4 ) << 10, 9, 6, 0.0000001 );
 
-        CvSVMParams params;
-        params.svm_type    = SVM::C_SVC;
-        params.C              = 0.1;
-        params.kernel_type = SVM::LINEAR;
-        params.term_crit   = TermCriteria(CV_TERMCRIT_ITER, (int)1e7, 1e-6);
+//        CvSVMParams params;
+//        params.svm_type    = SVM::C_SVC;
+//        params.C              = 0.1;
+//        params.kernel_type = SVM::LINEAR;
+//        params.term_crit   = TermCriteria(CV_TERMCRIT_ITER, (int)1e7, 1e-6);
     }
 }
