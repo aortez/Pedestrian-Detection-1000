c867901331eee9e4454214cc3b4c04ec1424392b
Allan Ortiz <aortez@gmail.com>
2011-12-12 01:46:14 -0800

    Added remaining input images to training set.

--- src/main.cpp
+++ src/main.cpp
@@ -36,20 +36,20 @@ int main( int argc, const char* argv[] )
     Mat src3 = imread( "test3.png" );
     Mat src4 = imread( "test4.png" );
 
-    detector.addTrainingImage( src1, true, true );
-//    detector.addTrainingImage( src2, true );
-//    detector.addTrainingImage( src3, true );
-//    detector.addTrainingImage( src4, true );
+    detector.addTrainingImage( src1, true );
+    detector.addTrainingImage( src2, true );
+    detector.addTrainingImage( src3, true );
+    detector.addTrainingImage( src4, true );
 
     Mat neg1 = imread( "neg1.png" );
     Mat neg2 = imread( "neg2.png" );
     Mat neg3 = imread( "neg3.png" );
     Mat neg4 = imread( "neg4.png" );
 
-    detector.addTrainingImage( neg1, false, true );
-//    detector.addTrainingImage( neg2, false );
-//    detector.addTrainingImage( neg3, false );
-//    detector.addTrainingImage( neg4, false );
+    detector.addTrainingImage( neg1, false );
+    detector.addTrainingImage( neg2, false );
+    detector.addTrainingImage( neg3, false );
+    detector.addTrainingImage( neg4, false );
 
     detector.train();
 
